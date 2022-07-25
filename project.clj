cd5eff6d4cfa5f83e1b93842ff601803b69c08f8
Rahul Gaur <rahul.nbg@gmail.com>
2022-07-25 23:42:00 +0530

    chore: version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.aregee/etlp "0.1.2"
+(defproject org.clojars.aregee/etlp "0.1.1"
   :description "Transducers based ETL processing pipeline"
   :url "https://github.com/aregee/etlp"
   :license {:name "MIT"
