716e7f51764da7b981745d3dcf8c7b25e50059ad
Rahul Gaur <rahul.nbg@gmail.com>
2022-08-23 23:51:51 +0530

    [wip] integrant system

--- src/etlp/core.clj
+++ src/etlp/core.clj
@@ -5,6 +5,7 @@
   (:import [java.io BufferedReader])
   (:gen-class))
 
+(def *etl-config (atom nil))
 
 (def create-pg-connection db/create-pg-connection)
 
@@ -16,20 +17,23 @@
 
 (def file-reducer reducers/file-reducer)
 
+
 (defn create-db-connection [config]
-  @(create-pg-connection config))
+  (prn config)
+  (create-pg-connection config))
 
 (defn create-db-writer [db]
   (fn [opts]
-    (create-pg-destination (:conn db) (:table-opts opts))))
+    (prn db opts)
+    (create-pg-destination db opts)))
 
 
-(defn create-json-stream [db config table-opts xform-provider reducers streams]
+(defn create-json-stream [{:keys [db config table-opts xform-provider reducers sinks]}]
   (fn [opts]
     (let [directory-reducer (:directory-reducer reducers)
-          sink ((get-in streams [:db-stream :sink]) table-opts)
+          sink ((:db-stream sinks) table-opts)
           compose-xf (fn [params]
-                       (comp (mapcat (:json-reducer reducers))   ;; Pipeline transducer
+                       (comp (mapcat ((:json-reducer reducers) opts))   ;; Pipeline transducer
                              (xform-provider params)
                              (map @sink)))]
       (directory-reducer {:pg-connection (:conn db) :pipeline compose-xf}))))
@@ -37,58 +41,56 @@
 
 (defn custom-file-reducer [xform-provider]
   (fn [opts]
+    (prn ">>>ivoked>>>>" opts)
     (fn [filepath]
       (prn filepath)
       (eduction
-       (xform-provider filepath opts) ; should return xform
+       (prn (xform-provider filepath opts))
        (reducers/read-lines filepath)))))
-
-
-(def *etl-config (atom nil))
-
-
-(defn ig-wrap-schema [params]
-  (fn []
-    (if-let [shape @*etl-config]
-      shape
-      (reset! *etl-config   {::config {:db {}
-                                       :kafka {}}
-                             ::db {:conn create-db-connection
-                                   :config (ig/ref ::config)}
-                             ::reducers {:directory-reducer create-pipeline-processor
-                                         :file-reducer file-reducer}
-
-                             ::streams  {:db-stream {:sink create-db-writer
-                                                     :db (ig/ref ::db)}}
-
-                             ::pipelines {}}))))
-
-(def schema (ig-wrap-schema {}))
-
-(schema)
 
 (defmulti etlp-component
   "Multi method to add extenstions to etlp"
   (fn [x]
     (get x :component)))
 
-(defmethod etlp-component ::pipelines [{:keys [id component ctx]}]
+(defmethod etlp-component ::processors [{:keys [id component ctx]}]
   (let [plugin {:run (:process-fn ctx)
                 :db (ig/ref ::db)
                 :config (ig/ref ::config)
                 :table-opts (:table-opts ctx)
                 :xform-provider (:xform-provider ctx)
                 :reducers (ig/ref ::reducers)
-                :streams (ig/ref ::streams)}]
-    (swap! *etl-config assoc-in [::pipelines (:name ctx)] plugin)))
+                :sinks (ig/ref ::sinks)}]
+    (swap! *etl-config assoc-in [::processors (:name ctx)] plugin)))
 
 (defmethod etlp-component ::reducers [{:keys [id component ctx]}]
   (let [plugin (custom-file-reducer (:xform-provider ctx))]
-    (swap! *etl-config assoc-in [::pipelines (:name ctx)] plugin)))
+    (swap! *etl-config assoc-in [::reducers (:name ctx)] plugin)))
+
+(defmethod etlp-component ::config [{:keys [id component ctx]}]
+  (let [plugin ctx]
+    (swap! *etl-config assoc-in [::config (:name plugin)] (dissoc plugin :name))))
 
 (defmethod etlp-component :default [params]
   (throw (IllegalArgumentException.
           (str "I don't know the " (get params :component) " support"))))
+
+(defn ig-wrap-schema [params]
+  (fn []
+    (if-let [shape @*etl-config]
+      shape
+      (reset! *etl-config   {::config {:db nil
+                                       :kafka nil}
+                             ::db {:conn create-db-connection
+                                   :config (ig/ref ::config)}
+                             ::reducers {:directory-reducer create-pipeline-processor
+                                         :file-reducer file-reducer}
+                             ::sinks  {:db-stream {:sink create-db-writer
+                                                   :db (ig/ref ::db)}}
+                             ::processors {}}))))
+
+(def schema (ig-wrap-schema {}))
+
 
 (def json-reducer-def {:id 1
                        :component ::reducers
@@ -96,9 +98,63 @@
                              :xform-provider (fn [filepath opts]
                                                (comp (map reducers/parse-line)))}})
 
+(def table-opts {:table :test_log_clj
+                 :specs  [[:id :serial "PRIMARY KEY"]
+                          [:type :varchar]
+                          [:field :varchar]
+                          [:created_at :timestamp
+                           "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]]})
+
 (def json-processor-def {:id 1
-                         :component ::pipelines
+                         :component ::processors
                          :ctx {:name :json-processor
                                :process-fn create-json-stream
-                               :table-opts {}
+                               :table-opts table-opts
                                :xform-provider (fn [ag] (comp (map prn)))}})
+
+
+(defn init [{:keys [components] :as params}]
+  (schema)
+  (etlp-component json-reducer-def)
+  (etlp-component json-processor-def)
+  ;; (prn (count components))
+  (loop [x (dec (count components))]
+    (when (>= x 0)
+      (etlp-component (nth components x))
+      (recur (dec x))))
+
+  (if-not (get-method ig/init-key ::config)
+    (defmethod ig/init-key ::config [_ {:keys [db kafka] :as opts}] opts))
+
+  (if-not (get-method ig/init-key ::db)
+  ;; Install this only if not already installed
+    (defmethod ig/init-key ::db [_ {:keys [conn config]
+                                    :as opts}]
+      (let [db (conn (:db config))]
+        (prn @db)
+        @db)))
+
+  (if-not (get-method ig/init-key ::reducers)
+  ;; Install this only if not already installed
+
+    (defmethod ig/init-key ::reducers [_ ctx]
+      ctx))
+
+  (if-not (get-method ig/init-key ::sinks)
+  ;; Install this only if not already installed
+
+    (defmethod ig/init-key ::sinks [_ {:keys [db-stream db]
+                                       :as opts}]
+    ;; (info topology)
+      (let [sink (:sink db-stream)]
+        (prn db)
+        (assoc opts :db-stream (sink (:conn db)))
+        )))
+
+  (if-not (get-method ig/init-key ::processors)
+    (defmethod ig/init-key ::processors [_ processors]
+      (reduce-kv (fn [acc k ctx]
+                   (assoc acc k ((:run ctx) (dissoc ctx :run)))) {} processors)))
+
+  (ig/init @*etl-config))
+
--- test/etlp/core_test.clj
+++ test/etlp/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [cheshire.core :as json]
             [clojure.java.io :as io]
-            [etlp.core :as etlp]))
+            [etlp.core :as etlp]
+            [etlp.reducers :as reducers]))
 
 
 
@@ -13,15 +14,15 @@
         0 {:type "string" :field (apply str (repeatedly 30 #(char (+ 33 (rand-int 90))))) }
         1 {:type "string" :field (apply str (repeatedly 30 #(char (+ 33 (rand-int 90)))))}
         2 {:type "empty"}))]
-      (with-open [f (io/writer "resources/fixtures/dummy.json")]
+      (with-open [f (io/writer "resources/dummy.json")]
         (binding [*out* f]
         (dotimes [_ 100000]
       (println (json/encode (rand-obj))))))))
 
 (def db-config {:host "localhost"
-                :user "test"
+                :user "postgres"
                 :dbname "test"
-                :password "test"
+                :password "postgres"
                 :port 5432})
 
 (def table-opts {
@@ -46,22 +47,51 @@
         (when (re-find #"a" string)
           (update log-entry :field str "-improved!")))))
 
-(def db (etlp/create-pg-connection db-config))
+;; (def db (etlp/create-pg-connection db-config))
 
-(def bulk-json-writer (etlp/create-pg-destination @db table-opts))
+;; (def bulk-json-writer (etlp/create-pg-destination @db table-opts))
 
-(defn- pipeline [params]
-  (comp (mapcat etlp/json-reducer)   ;; Pipeline transducer
-        (filter valid-entry?)
-        (keep transform-entry-if-relevant)
-        (partition-all 1000)
-        (map @bulk-json-writer)))
+;; (defn- pipeline [params]
+;;   (comp (mapcat etlp/json-reducer)   ;; Pipeline transducer
+;;         (filter valid-entry?)
+;;         (keep transform-entry-if-relevant)
+;;         (partition-all 100)
+;;         (map @bulk-json-writer)))
 
-(def json-processor (etlp/create-pipeline-processor {:pg-connection @db :pipeline pipeline}))
+;; (def json-processor (etlp/create-pipeline-processor {:pg-connection @db :pipeline pipeline}))
 
-(defn exec-fp [{:keys [path days]}]
-  (json-processor {:params 1 :path path}))
+;; (defn exec-fp [{:keys [path days]}]
+;;   (json-processor {:params 1 :path path}))
 
-(deftest e-to-e-test
-  (testing "etlp/create-pipeline-processor should execute without error"
-    (is (= true true))))
+;; (gen-files)
+
+
+;; (def json-reducer-def {:id 1
+;;                        :component :etlp.core/reducers
+;;                        :ctx {:name :json-reducer
+;;                              :xform-provider (fn [filepath opts]
+;;                                                (comp (map reducers/parse-line)))}})
+
+;; (def json-processor-def {:id 1
+;;                          :component :etlp.core/pipelines
+;;                          :ctx {:name :json-processor
+;;                                :process-fn etlp/create-json-stream
+;;                                :table-opts {}
+;;                                :xform-provider (fn [ag] (comp (map prn)))}})
+
+(def db-config-def {:id 1
+                    :component :etlp.core/config 
+                    :ctx (merge {:name :db} db-config)})
+
+
+(def etlp-app (etlp/init {:components [db-config-def]}))
+(def json-processor (get-in etlp-app [:etlp.core/processors :json-processor]))
+;; (clojure.pprint/pprint json-processor)
+(def processor (json-processor {:days 1}))
+(processor {:path "resources/" :params 1})
+;; (deftest e-to-e-test
+;;   (testing "etlp/create-pipeline-processor should execute without error"
+;;     (is (= true (exec-fp {:path "resources/" :days 1})))))
+;; (deftest e-to-e-test
+;;   (testing "etlp/create-pipeline-processor should execute without error"
+;;     (is (= nil nil ))))
