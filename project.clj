c2ac6e2317c8c0b2d4b7fed84f004ea9816a7b49
Rahul Gaur <rahul.nbg@gmail.com>
2023-05-02 21:13:34 +0530

    fix: publish tweaks scope

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.aregee/etlp "0.4.0"
+(defproject com.github.aregee/etlp "0.4.0-SNAPSHOT"
   :description "Transducers based ETL processing pipeline"
   :url "https://github.com/aregee/etlp"
   :license {:name "MIT"
