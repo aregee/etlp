11e6e51951e5f1f70cbabb5652d0b8da7de39a2f
Rahul Gaur <rahul.nbg@gmail.com>
2023-05-03 20:05:47 +0530

    fix: visulalizer issue

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.aregee/etlp "0.4.0-SNAPSHOT"
+(defproject com.github.aregee/etlp "0.4.1-SNAPSHOT"
   :description "Transducers based ETL processing pipeline"
   :url "https://github.com/aregee/etlp"
   :license {:name "MIT"
